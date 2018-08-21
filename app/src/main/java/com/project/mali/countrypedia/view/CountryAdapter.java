c5218af1b57ae7c22f8f1f8a622118677916d23a
m.pouraskari <m.pouraskari@digikala.com>
2018-08-21 09:59:35 +0430

    country commit

--- app/src/main/java/com/project/mali/countrypedia/view/CountryAdapter.java
+++ app/src/main/java/com/project/mali/countrypedia/view/CountryAdapter.java
@@ -3,14 +3,11 @@ package com.project.mali.countrypedia.view;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.bumptech.glide.Glide;
 import com.project.mali.countrypedia.R;
 import com.project.mali.countrypedia.data.local.entity.CountryEntity;
 
